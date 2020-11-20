7a5eb196174f25aca8c4fd3492f22072b7101c56
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-11-20 19:54:55 -0300

    some https requests with firebase

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Post } from "./post.model";
+import { PostsService } from "./posts.service"
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
